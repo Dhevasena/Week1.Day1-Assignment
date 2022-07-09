496ed24b0fab7f5b7babe4ae5f64fc21e079f081
Dhevasena <108989186+Dhevasena@users.noreply.github.com>
2022-07-09 20:24:29 +0530

    Add files via upload

---
+++ Fibbinocci.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Fibbinocci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int firstNum=0,secNum=1,sum=0;
+System.out.println(firstNum);
+for (int i=0; i<=11; i++){
+	
+	sum=firstNum+secNum;
+	System.out.println(sum);
+    firstNum=secNum;
+	secNum=sum;	
+	
+}
+	
+	}
+
+}
